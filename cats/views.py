28b5944bd3623ba9c3303577ea4fe76f9e776831
Максим Кужненков <maksim-kuzhnenkov@yandex.ru>
2023-08-17 16:08:47 +0300

    permissions

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from .permissions import OwnerOrReadOnly
 
 from .models import Achievement, Cat, User
 
@@ -8,7 +9,7 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (OwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
