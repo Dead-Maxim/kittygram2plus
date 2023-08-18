ca76fecc80b493e5c4cf5830c3ba8d4455f829fd
Максим Кужненков <maksim-kuzhnenkov@yandex.ru>
2023-08-18 21:42:31 +0300

    пагинация и тротлинг

--- cats/permissions.py
+++ cats/permissions.py
@@ -10,4 +10,11 @@ class OwnerOrReadOnly(permissions.BasePermission):
             )
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
+
+
+class ReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
--- cats/views.py
+++ cats/views.py
@@ -1,18 +1,30 @@
 from rest_framework import viewsets
 from .permissions import OwnerOrReadOnly
-
+from rest_framework.throttling import ScopedRateThrottle
+from .throttling import WorkingHoursRateThrottle
+# from rest_framework.pagination import PageNumberPagination
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .pagination import CatsPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (ScopedRateThrottle, WorkingHoursRateThrottle,)
+    throttle_scope = 'low_request'
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    pagination_class = CatsPagination
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('^name',)
+    ordering_fields = ('name', 'birth_year') 
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
